ed64d6e3fe5b48565b45c9cee9a181b68b3ae547
BohdanStelmashchuk <bohdan.stelmashchuk@jetsoftpro.com>
2021-03-30 23:35:39 +0300

    MB-4 Add date

--- FootPredict/app/src/main/java/com/example/footpredict/LeaguesActivity.kt
+++ FootPredict/app/src/main/java/com/example/footpredict/LeaguesActivity.kt
@@ -1,25 +1,58 @@
 package com.example.footpredict
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.footpredict.adapters.LeagueAdapter
 import com.example.footpredict.models.League
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LeaguesActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leagues)
 
+        initializeRecyclerView()
+        setCurrentDate()
+    }
+
+    fun fillArray(leagues: ArrayList<League>){
+        leagues.add(League(1, "English Premier League"))
+        leagues.add(League(2, "Bundesliga"))
+        leagues.add(League(3, "Seria A"))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setCurrentDate(){
+        var tvDate = findViewById<TextView>(R.id.tvDate)
+        var currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        val sdf = SimpleDateFormat("EEEE")
+        val d = Date()
+        val dayOfTheWeek: String = sdf.format(d)
+        tvDate.text = currentDate.toString() + ", " + dayOfTheWeek
+    }
+
+    fun initializeRecyclerView(){
         var leagues = ArrayList<League>()
-        leagues.add(League(1,"English Premier League"))
-        leagues.add(League(2,"Bundesliga"))
-        leagues.add(League(3,"Seria A"))
+        fillArray(leagues)
 
         var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.hasFixedSize()
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LeagueAdapter(leagues,this)
+        recyclerView.adapter = LeagueAdapter(leagues, this)
+    }
+
+    public fun onBack(view: View) {
+        onBackPressed()
     }
 }
